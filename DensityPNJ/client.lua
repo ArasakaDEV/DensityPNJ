30e62503ac5542d8c4cdc482a9cb1a7cc838a467
Arasaka DEV <106636546+ArasakaDEV@users.noreply.github.com>
2022-06-01 03:18:41 +0200

    Add files via upload

---
+++ DensityPNJ/client.lua
@@ -0,0 +1,14 @@
+--- Arasaka Dev
+--- https://discord.gg/4WUwCzEAJf
+
+DensityMultiplier = 0.0 -- [0.0 - 1.0]
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        SetVehicleDensityMultiplierThisFrame(DensityMultiplier)
+        SetPedDensityMultiplierThisFrame(DensityMultiplier)
+        SetRandomVehicleDensityMultiplierThisFrame(DensityMultiplier)
+        SetParkedVehicleDensityMultiplierThisFrame(DensityMultiplier)
+        SetScenarioPedDensityMultiplierThisFrame(DensityMultiplier, DensityMultiplier)
+    end
+end)
